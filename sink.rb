de1387dc43c3d3c7bfb40205a61b58d94de77b0a
Coby Chapple <coby@cobychapple.com>
2014-07-30 12:01:00 +0100

    Auto-sync: M - sink.rb

--- sink.rb
+++ sink.rb
@@ -20,37 +20,39 @@ def dir_is_syncable(dir)
   end
 end
 
-github = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
+if dir_is_syncable(Dir.pwd)
+  github = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 
-while dir_is_syncable(Dir.pwd) do
-  g = Git.open(Dir.pwd)
+  while true do
+    g = Git.open(Dir.pwd)
 
-  g.status.each do |file|
-    if file.untracked || !file.type.nil?
+    g.status.each do |file|
+      if file.untracked || !file.type.nil?
 
-      if file.type == "D"
-        g.remove file.path
-      else
-        g.add file.path
-      end
-
-      message =
-        if file.untracked
-          # Untracked files don’t have a type of 'A' before being staged,
-          # and at the point we run `status` we haven’t staged anything yet.
-          "Auto-sync: A - #{file.path}"
+        if file.type == "D"
+          g.remove file.path
         else
-          "Auto-sync: #{file.type} - #{file.path}"
+          g.add file.path
         end
 
-      puts "#{message}. Syncing…"
-      g.commit message
-      g.push('origin')
-      puts "Done."
+        message =
+          if file.untracked
+            # Untracked files don’t have a type of 'A' before being staged,
+            # and at the point we run `status` we haven’t staged anything yet.
+            "Auto-sync: A - #{file.path}"
+          else
+            "Auto-sync: #{file.type} - #{file.path}"
+          end
+
+        puts "#{message}. Syncing…"
+        g.commit message
+        g.push('origin')
+        puts "Done."
+      end
     end
+
+    g.pull('origin')
+
+    sleep 1
   end
-
-  g.pull('origin')
-
-  sleep 1
 end
