37444ef7f9b2b11fc9c51df0eaa93404289ae348
Coby Chapple <coby@cobychapple.com>
2014-07-30 15:41:54 +0100

    Auto-sync: 1 file changed. - M  lib/sink.rb

--- lib/sink.rb
+++ lib/sink.rb
@@ -15,6 +15,28 @@ class Sink
     def setup_sync
 
     end
+
+    def dir_is_syncable(dir)
+      begin
+        g = Git.open(dir)
+        origin_exists = false
+        g.remotes.each do |remote|
+          origin_exists = true if !origin_exists && remote.name == 'origin' && !remote.url.match(/github/).nil?
+        end
+        origin_exists
+      rescue ArgumentError
+        puts "Not a git repo, sorry!"
+        false
+      end
+    end
+
+    def nwo_of_origin(remotes)
+      remotes.select { |r| r.name == 'origin' }
+        .first.url
+        .gsub(/https:\/\/github.com\//, '')
+        .gsub(/\.git/, '')
+    end
+
   end
 
 end
